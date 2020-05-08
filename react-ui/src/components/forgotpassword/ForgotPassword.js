3d15fb746b22129ae0aa60dc3064ee49f58035f1
Amir Mursal <amir.mursal@communitybrands.com>
2020-05-08 13:37:16 +0530

    message fix for forgot password

--- react-ui/src/components/forgotpassword/ForgotPassword.js
+++ react-ui/src/components/forgotpassword/ForgotPassword.js
@@ -40,6 +40,7 @@ export default class ForgotPassword extends React.Component {
         } else {
           this.setState({
             email: "",
+            message: null,
             error: true,
           });
         }
